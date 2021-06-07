cc4435c7906c9fcbb308c4caa43278bfd97931e3
VictorBortoli <victorferreiradebortoli@gmail.com>
2021-06-07 16:11:57 -0300

    terminanado o menu com objetos

--- src/br/com/senai/pessoa/ProgramaPrincipal.java
+++ src/br/com/senai/pessoa/ProgramaPrincipal.java
@@ -1,10 +1,48 @@
 package br.com.senai.pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		List<Pessoa> pessoas = new ArrayList<>();
+		
+		
+		PessoaController pessoacontroller = new PessoaController();
+		
+		boolean sair = false;
+				
+		
+		do {
+			
+			pessoacontroller.menu();
+			
+			int opcao = pessoacontroller.LeOpcao();
+			
+			switch(opcao) {
+				case 1:
+					pessoas.add(pessoacontroller.cadastrarPessoa());
+					break;
+					
+				case 2:
+					pessoacontroller.listarPessoas(pessoas);
+					break;
+					
+				case 9:
+					sair = true;
+					break;
+				
+				default:
+					System.out.println("Opção invalida");
+					break;
+			}
+			
+		}while(!sair);
+		
+		
+		System.out.println("Sistema finalizado");
 	}
 
 }
