3467485f2726a96b2bd9694b536901218c5bf794
VictorBortoli <victorferreiradebortoli@gmail.com>
2021-06-10 16:37:06 -0300

    REfatoração da controller para adicionar pessoa

--- src/br/com/senai/pessoa/PessoaController.java
+++ src/br/com/senai/pessoa/PessoaController.java
@@ -3,61 +3,98 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PessoaController {
-	
+
 	private Scanner tec;
 	
 	public PessoaController(){
 		tec = new Scanner(System.in);
 	}
-	public int LeOpcao() {
-		System.out.print("> ");
+	
+	public int leOpcao(){
+		System.out.print("Informe a opção desejada -> ");
 		return tec.nextInt();
 	}
 	
-	
 	public void menu() {
-		System.out.println("\n--- MENU ---");
-		System.out.println("(1) Cadastrar pessoa");
-		System.out.println("(2) Lista de pessoas Cadastradas;");
-		System.out.println("(9) Sair do sistema");
-		System.out.println("--------------------");
+		System.out.println("|-------------- MENU --------------|");
+		System.out.println("|1 -> Cadastrar Pessoas            |");
+		System.out.println("|2 -> Lista de Pessoas Cadastradas |");
+		System.out.println("|9 -> Sair do Sistema              |");
+		System.out.println("|----------------------------------|");
+		System.out.println("\n");
 	}
 	
-	public Pessoa cadastrarPessoa(){
+	public Pessoa cadastrarPessoa() {
 		Pessoa pessoa = new Pessoa();
 		
-		System.out.println("--- CADASTRAR PESSOA ---");
+		System.out.println("---- Cadastrar Pessoas ----");
+		System.out.println("\n");
 		
-		System.out.print("Informe o nome: ");
+		System.out.print("Informe o seu nome: ");
 		tec.nextLine();
 		pessoa.setNome(tec.nextLine());
 		
-		System.out.print("informe o ano de nascimento: ");
+		System.out.print("Informe o seu Ano de Nascimento: ");
 		pessoa.setAnoDeNascimento(tec.nextInt());
 		
-		System.out.print("Informe a altura: ");
+		System.out.print("Informe a sua altura: ");
 		pessoa.setAltura(tec.nextDouble());
 		
+		System.out.print("Informe o país: ");
+		tec.nextLine();
+		pessoa.setNomeDoPais(tec.nextLine());
 		
+		System.out.print("Informe a sigla do Páis: ");
+		pessoa.setUfDoPais(tec.nextLine());
 		
+		System.out.print("Informe o Estado: ");
+		pessoa.setNomeDoEstado(tec.nextLine());
+		
+		System.out.print("Informe a sigla do Estado: ");
+		pessoa.setUfDoEstado(tec.nextLine());
+		
+		System.out.print("Informe o nome da Cidade: ");
+		pessoa.setNomeDaCidade(tec.nextLine());
+		
+		System.out.print("Informe a Rua: ");
+		pessoa.setRua(tec.nextLine());
+		
+		System.out.print("Informe o Bairro: ");
+		pessoa.setBairro(tec.nextLine());
+		
+		System.out.print("Informe o numero da casa: ");
+		pessoa.setNumero(tec.nextLine());
+		
+		System.out.print("Informe o complemento: ");
+		pessoa.setComplemento(tec.nextLine());
 		return pessoa;
-		
 	}
 	
 	public List<Pessoa> listarPessoas(List<Pessoa> pessoas){
-		System.out.println("Pessoas Cadastradas");
+		System.out.println("\n");
+		System.out.println("---- PESSOAS CADASTRADADS ----");
+		System.out.println("\n");
 		
-		System.out.printf("| %20s | %4s | %5s | %6s | \n", "Nome", "Ano", "Idade", "Altura");
+		System.out.printf("| %20s | %4s | %5s | %4s | %10s | %10s | %20s | %10s | %20s | %10s | %10s | %10s | %10s |\n" , 
+				"Nome" , "Ano" , "Idade" , "Altura", "País", "Sigla do pais", "Estado", "Sigla do Estado", "Nome da Cidade","Rua","Bairro","Numero da casa","Complemento");
 		
 		for(int i = 0; i < pessoas.size(); i++) {
-			System.out.printf("| %20s | %4d | %5d | %4.2f",	
-				pessoas.get(i).getNome(),
-				pessoas.get(i).getAnoDeNascimento(),
-				pessoas.get(i).getIdade(),
-				pessoas.get(i).getAltura());
+			System.out.printf("| %20s | %4d | %5d | %6.2f | %10s | %10s | %20s | %10s | %20s | %10s | %10s | %10s | %10s |\n" , 
+					pessoas.get(i).getNome(),
+					pessoas.get(i).getAnoDeNascimento(),
+					pessoas.get(i).getIdade(),
+					pessoas.get(i).getAltura(),
+					pessoas.get(i).getNomeDoPais(),
+					pessoas.get(i).getUfDoPais(),
+					pessoas.get(i).getNomeDoEstado(),
+					pessoas.get(i).getUfDoEstado(),
+					pessoas.get(i).getNomeDaCidade(),
+					pessoas.get(i).getRua(),
+					pessoas.get(i).getBairro(),
+					pessoas.get(i).getNumero(),
+					pessoas.get(i).getComplemento());
 		}
+		System.out.println("\n");
 		return pessoas;
 	}
-	
-
 }
