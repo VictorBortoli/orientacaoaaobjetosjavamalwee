f652301f3982ceff344e5a6c968fe172bcb9fdbe
VictorBortoli <victorferreiradebortoli@gmail.com>
2021-06-14 16:41:56 -0300

    refatoração de classe

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -1,24 +1,30 @@
-package br.com.senai.pessoa;
+package br.com.senai;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import br.com.senai.pessoa.Pessoa;
+import br.com.senai.pessoa.PessoaController;
+import br.com.senai.produto.Produto;
+import br.com.senai.produto.ProdutoController;
 
 public class ProgramaPrincipal {
 	
 	public static void main(String[] args) {
 		
 		List<Pessoa> pessoas = new ArrayList<>();
+		List<Produto> produtos = new ArrayList<Produto>();
 		
 		PessoaController pessoaController = new PessoaController();
-		
+		ProdutoController produtoController = new ProdutoController();
 		boolean sair = false;
 		
 		do {
 			
 			pessoaController.menu();
-			
 			int opcao = pessoaController.leOpcao();
-					
+//			int opcao2 = produtoController.leOpcao();	
+//			
 			switch(opcao) {
 			case 1:
 				System.out.println("\n");
@@ -26,12 +32,27 @@ public class ProgramaPrincipal {
 				break;
 				
 			case 2:
-				if(pessoaController.listarPessoas(pessoas).isEmpty()) {
+				if(pessoas.isEmpty()) {
 					System.out.println("A lista está vazia, cadastre alguma pessoa!");
 					System.out.println("\n");
 				}
 				else{
 					pessoaController.listarPessoas(pessoas);
+				}
+				break;
+			
+			case 3:
+				System.out.println("\n");
+				produtos.add(produtoController.cadastrarProduto());
+				break;
+				
+			case 4:
+				if(produtos.isEmpty()) {
+					System.out.println("A lista está vazia, cadastre um produto");
+					System.out.println("/n");
+				}
+				else {
+					produtoController.listarProdutos(produtos);
 				}
 				break;
 			case 9:
